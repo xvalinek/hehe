28d5d62ca02bd82a659fd14042b7cfb12830ef55
xvalinek <93054051+xvalinek@users.noreply.github.com>
2022-03-29 16:08:33 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
+
+app = Flask(__name__)
+app.secret_key = "chuj"
+app.permanent_session_lifetime = timedelta(minute=5)
+
+@app.route("/")
+def home():
+    return "<h1>test w h1</h1>"
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["login"]
+        session["user"] = user
+        return redirect(url_for("podstrona"))
+    else:
+        if "user" in session: 
+            return redirect(url_for("podstrona"))
+        return render_template("login.html")
+
+
+@app.route("/user")
+def podstrona():
+    if "user" in session:
+        user = session["user"]
+        return f"sesja: {user}"
+    else:
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
